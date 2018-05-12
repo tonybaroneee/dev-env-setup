ee4bccf4c40b8d21c82787f3cfe2a0d71bed0d7b
Anthony Barone <tonybaroneee@gmail.com>
2018-05-12 08:01:25 -0400

    Add updated configs, clean up

--- README.md
+++
@@ -1,3 +0,0 @@
-To run:
-
-`sh bootstrap.sh`
---
+++ config.fish
@@ -0,0 +1,10 @@
+alias gb="git branch"
+alias gd="git diff"
+alias gl="git log"
+alias gs="git status"
+alias gp="git pull"
+alias ga="git commit --amend --no-edit"
+alias gpf="git push -f"
+alias gup="git fetch upstream; and git merge upstream/master"
+
+set -g fish_user_paths "/usr/local/opt/node@8/bin" $fish_user_paths
